14fe052f0055292793b5e3ea76eab7d1651f15de
J Paul Walker <jpaulwalker@users.noreply.github.com>
2022-08-01 21:44:18 +0000

    learning about input function

---
+++ inputting.py
@@ -0,0 +1,3 @@
+x=input("what do you want x to be?")
+print(x)
+
---
+++ lab_input/iptaker02.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+def main():
+    user_input=input("please enter an IPV4 ip address:")
+    print("you told me the ipv4 address is:", user_input)
+    vendor_name=input("what is the vendor's name")
+    print("the vendor's name is", vendor_name)
+main()
+

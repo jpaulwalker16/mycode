7b87364faa8b527cd5e998be966e7a8eaf767e6a
J Paul Walker <jpaulwalker@users.noreply.github.com>
2022-08-02 17:03:37 +0000

    mixed lists

---
+++ mix-list/mixlist01.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+my_list=['192.168.0.5', 5060, 'UP']
+
+#references the first item in the list my_list
+print('the first item in the list (IP): ' + my_list[0])
+
+#references 2nd item in list
+print('the second item in the list (port): ' + str(my_list[1]))
+
+#references 3rd item in the list
+print('the last item in the list (state): ' + my_list[2])
+

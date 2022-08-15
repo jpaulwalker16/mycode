7b120289966f68871d95577b66d7be720b7331e0
Paul Walker <jpaulwalker16@users.noreply.github.com>
2022-08-15 13:44:28 +0000

    reading data challenge

--- challenges/reading_data_project.py
+++ challenges/reading_data_project.py
@@ -3,23 +3,56 @@
 import csv
 #import readlines
 
+#defining the main function for the program
 def main():
 
     #open file to read contents
-    carscontents = open("cars.csv","r")
-    #if f.mode == "r":
-    #    carscontents = f.read() 
-        #print(carscontents)
+    with open("cars.csv","r") as carscontents:
 
 #read off the first three lines of data and separate them
-    first3lines = carscontents.readline().split(",")
-        print("Enter in which contents you want listed or enter in any data you have about what car you are looking up; see the following choices")
-    for x in first3lines:
-        print(
-        
-    with open("cars.csv") as carscontents:
-        for x in csv.DictReader(carscontents):
-            print(x[VIN])
+        datatitles = carscontents.readline().split(",")
+
+#split off the rest of the lines of data and separate them
+        datacontent = carscontents.readlines()[1:]  # readlines converts a fileobj to a list, so the split is redundant
+        #ask for input on data points
+        answer = input("Welcome to cars.com! Do you have any specific data point on your vehicle? (y/n)")
+        if answer.lower() == "y":
+
+            #give the following choices for data points
+            print("Here are the following choices:")
+                for a,b,c in zip(datatitles[::3],datatitles[1::3],datatitles[2::3]):
+                    print('{:<30}{:<30}{:<}'.format(a,b,c))
+
+            #ask for input of a category
+              answer1 = input("Input a parameter to list all the vehicles with that information.\n")
+              with open("cars.csv","r") as carscontents1:
+                  #for x in csv.DictReader(carscontents1):
+
+                  #search each line for the answer1 content and print out the whole line
+                    c = datacontent.search(answer1)
+                    if c:
+                        print(c[answer1])
+
+           # print("Here are the following choices:")
+           # for a,b,c in zip(datatitles[::3],datatitles[1::3],datatitles[2::3]):
+           #         print('{:<30}{:<30}{:<}'.format(a,b,c))
+
+       #print the following categories of choices    
+        else:
+            print("Here are the following choices:")
+            for a,b,c in zip(datatitles[::3],datatitles[1::3],datatitles[2::3]):
+                print('{:<30}{:<30}{:<}'.format(a,b,c))
+                    
+        # vertically
+        #for x in datatitles:
+           # print(x)
+
+        # horizontally
+        #print(" ".join(datatitles))
+        #if answer2 in datatitles:
+    with open("cars.csv","r") as carscontents2:
+        for x in csv.DictReader(carscontents2):
+                print(x[answer2])
 
 if __name__== "__main__":
     main()
